e5f435a68905ea91e008d1b38465460564e654dd
unknown <monimul2014@gmail.com>
2019-10-02 11:57:31 +0600

    added test file

---
+++ MyConsoleApp/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+           String tushar = "";
+            Console.WriteLine("Please, Enter Your Name");
+            tushar = Console.ReadLine();
+
+            Console.WriteLine("Hello:" + tushar);
+            Console.ReadKey();
+        }
+    }
+}
